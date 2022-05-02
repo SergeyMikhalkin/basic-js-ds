bf3dad728c3aa921a03f1e920f775d260f684646
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-05-02 21:12:18 +0300

    feat: implement remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  while (l.value === k) {
+    if (l.value === k) {
+      l = l.next;
+    }
+  }
+  let curr = l;
+  while(curr.next != null) {
+    if(curr.next.value === k) {
+      if(curr.next.next !== null) {
+        curr.next = curr.next.next;
+        continue;
+      }
+      else {
+        curr.next = null;
+        continue;
+      }
+    }
+    curr = curr.next;
+  }
+
+  return l;
 }
 
 module.exports = {
